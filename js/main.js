259bce65e74681927d88b6d7c162fdf5fc5d2032
Kristoffer <kristoffer.benckert@chasacademy.se>
2024-12-05 15:21:24 +0100

    fixes sort function

--- js/main.js
+++ js/main.js
@@ -4,49 +4,47 @@ const productSection = document.querySelector(".product_section");
 const categoryUl = document.querySelector(".category_list");
 const sortBy = document.querySelector("#sort_by");
 
+let filterBy = "all";
+let compare;
 
-categoryUl.addEventListener("click", function (event) {
-  if (event.target.tagName === "LI") {
-    let filterBy = event.target.textContent.toLowerCase();
-    displayProducts(filterBy);
-  }
-});
-
-// sortBy.addEventListener("change", displayProducts(sortBy));
-
-const displayProducts = async (filterBy) => {
+const displayProducts = async () => {
   const allProducts = await getData();
-  //   console.log(allProducts);
-  let compare;
 
-//   if (sortBy.value === "lowest") {
-//     compare = (a, b) => a.price - b.price
-// } else if (sortBy.value === "highest") {
-//     compare = (a, b) => b.price - a.price
-// }
-
-  const productList = allProducts
-    .sort(sortBy.value === "highest" ? (a, b) => b.price - a.price
-        : sortBy.value === "lowest" ? (a, b) => a.price - b.price
-        : NaN)
+    const productList = allProducts
+    .sort(compare)
     .filter(product => filterBy !== "all" ? product.category === filterBy : true)
     .map(
       (product) =>
-        `
-        <article class="product">
-            <img src="${product.image}" alt="">
-            <div class="product_div">
-                <div class="product_text">
-                    <h2 class="product_name">${product.title}</h2>
-                    <p class="product_price">$${product.price}</p>
-                </div>
-                <button class="product_buy">Varukorg</button>
-            </div>
-        </article>`
-    )
+      `
+      <article class="product">
+      <img src="${product.image}" alt="">
+      <div class="product_div">
+      <div class="product_text">
+      <h2 class="product_name">${product.title}</h2>
+      <p class="product_price">$${product.price}</p>
+      </div>
+      <button class="product_buy">Varukorg</button>
+      </div>
+      </article>
+      `)
     .join("");
-  productSection.innerHTML = productList;
-  console.log(productList);
-};
+    productSection.innerHTML = productList;
+  };
+    
+  categoryUl.addEventListener("click", function (event) {
+    if (event.target.tagName === "LI") {
+      filterBy = event.target.textContent.toLowerCase();
+      displayProducts();
+    }
+  });
+  
+  sortBy.addEventListener("change", function () {
+  compare = sortBy.value === "highest" ? (a, b) => b.price - a.price
+          : sortBy.value === "lowest" ? (a, b) => a.price - b.price
+          : () => NaN;
 
-displayProducts("all");
+  displayProducts();
+  });
+
+  displayProducts();
+  
