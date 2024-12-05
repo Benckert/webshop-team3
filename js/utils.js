4439a4d24db3ce4d13f57bf93af1113fe86dad14
Kristoffer <kristoffer.benckert@chasacademy.se>
2024-12-05 10:13:55 +0100

    adds fetch function

---
+++ js/utils.js
@@ -0,0 +1,14 @@
+async function getData(data) {
+  const url = `https://fakestoreapi.com/${data}`;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+}
