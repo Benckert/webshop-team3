26c1117663bdc47aa7c3e1b51fccf5a107cda7ef
Kristoffer <kristoffer.benckert@chasacademy.se>
2024-12-11 00:12:04 +0100

    adds item counter

--- js/main.js
+++ js/main.js
@@ -4,45 +4,86 @@ const productSection = document.querySelector(".product-section");
 const categoryUl = document.querySelector(".category_list");
 const sortBy = document.querySelector("#sort_by");
 
+sortBy.value = "none";
+
 let filterBy = "all";
 let compare;
+// let compare = () => NaN;
 
-const displayProducts = async () => {
-  const allProducts = await getData();
+let allProducts;
 
-    const productList = allProducts
-    .sort(compare)
-    .filter(product => filterBy !== "all" ? product.category === filterBy : true)
-    .map(product =>
-      `
-      <article class="product">
-        <div class="product__img">
-          <img src="${product.image}" alt="Product image">
-        </div>
-        <div class="product__content">
-          <h2 class="product__title">${product.title}</h2>
+const loadProducts = async () => {
+  allProducts = await getData();
+  displayProducts();
+  addListeners();
+}
+
+// const displayProducts = async () => {
+const displayProducts = () => {
+  // const allProducts = await getData();
+
+  const productList = allProducts
+  .sort(compare)
+  .filter(product => filterBy !== "all" ? product.category === filterBy : true)
+  .map(product =>
+    `
+    <article class="product">
+    <div class="product__img">
+        <img src="${product.image}" alt="Product image">
+      </div>
+      <div class="product__content">
+        <h2 class="product__title">${product.title}</h2>
+        <div class="product__item-to-cart">
           <p class="product__price">$${product.price}</p>
-          <button class="product__shopping-cart">Varukorg</button>
+          <div id="item-${product.id}" class="product__cart-buttons">
+            <button class="product__remove-from-cart" type="button">-</button>
+            <span class="product__count">0</span>
+            <button class="product__add-to-cart" type="button">+</button>
+          </div>
         </div>
-      </article>
-      `)
-    .join("");
-    productSection.innerHTML = productList;
-  };
+      </div>
+    </article>
+    `)
+  .join("");
+  productSection.innerHTML = productList;
+};
 
-  categoryUl.addEventListener("click", function (event) {
-    if (event.target.tagName === "LI") {
-      filterBy = event.target.textContent.toLowerCase();
-      displayProducts();
-    }
-  });
+//
 
-  sortBy.addEventListener("change", function () {
-  compare = sortBy.value === "highest" ? (a, b) => b.price - a.price
-          : sortBy.value === "lowest" ? (a, b) => a.price - b.price
-          : () => NaN;
+function addListeners() {
+  allProducts.forEach(element => {
+    document.getElementById(`item-${element.id}`).addEventListener("click", count);
+  })
 
-  displayProducts();
-  });
+  // document.getElementsByClassName("product__remove-from-cart")
+}
 
-  displayProducts();
+function count(e) {
+  if (e.target.classList.contains("product__remove-from-cart")) {
+    if (parseInt(e.target.nextElementSibling.textContent) > 0)
+      e.target.nextElementSibling.textContent--
+  }
+  if (e.target.classList.contains("product__add-to-cart")) {
+    e.target.previousElementSibling.textContent++
+  }
+}
+
+//
+
+categoryUl.addEventListener("click", function (event) {
+  if (event.target.tagName === "LI") {
+    filterBy = event.target.textContent.toLowerCase();
+    displayProducts();
+  }
+});
+
+sortBy.addEventListener("change", function () {
+compare = sortBy.value === "highest" ? (a, b) => b.price - a.price
+        : sortBy.value === "lowest" ? (a, b) => a.price - b.price
+        : () => NaN;
+
+displayProducts();
+});
+
+loadProducts();
+// displayProducts();
